f9f0808e4aea038015cbc76cf33c1a5cb010145e
Carlos Garcia <ambro_93garcas@hotmail.com>
2023-10-17 13:37:32 -0600

    feat imagenfield & mediafiles settings

--- library/settings.py
+++ library/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app_book',
+    'imagekit',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +121,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+
+#MEDIA_FILES
+MEDIA_ROOT= os.path.join(BASE_DIR, 'mediafiles')
+MEDIA_URL= '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- library/urls.py
+++ library/urls.py
@@ -1,3 +1,6 @@
+
+
+
 """
 URL configuration for library project.
 
@@ -17,7 +20,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+#MEDIAFILES####
+from django.conf import settings
+from django.conf.urls.static import static
+################
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('app_book.urls')),
 ]
+
+#####MEDIAFILES#####
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#################
